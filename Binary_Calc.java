a06a4e2d10b24f8a22ddc4c81155e85b9e01f186
d02ev <vikramadityapratapsingh98@gmail.com>
2020-09-14 14:30:12 +0530

    Added Binary_Calc.java
    Added the file containing the program

---
+++ Binary_Calc.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Binary_Calc {
+
+    public static void main(String[] args) {
+
+        // extra variables and arrays
+        int n;
+        int i = 1;
+        int[] code = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        Scanner userInput = new Scanner(System.in);
+
+        // number input
+        int decimalNumber = userInput.nextInt();
+
+        // storing first iteration int the array
+        code[0] = decimalNumber % 2;
+
+        // storing decimalNumber into another variable n for manipulations
+        n = decimalNumber;
+
+        // algorithm
+        while(n != 0) {
+
+            n /= 2;
+            code[i] = n % 2;
+            i++;
+        
+        }
+
+        for(int j = 9; j >= 0; j--) {
+
+            System.out.print("[" + code[j] + " ]");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Binary_Calculator
+
  A basic binary calculator
+
+## Summary
+
+ This program calculates the binary form of a number
